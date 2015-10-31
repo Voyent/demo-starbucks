73e1c22b4a4624a2a4336e6d7860356f2189fb8b
philipbreau <philip.breau@icesoft.com>
2015-10-31 10:26:59 -0300

    stop deploy if gulp errors

--- deploy-starbucks.sh
+++ deploy-starbucks.sh
@@ -1,17 +1,15 @@
 #!/bin/bash  
+set +e
 
 HOST=web1d
 
 #check host and api in config
 rm -rf ./dist
-gulp
-# TODO fix gulpfile.js to add admin.js and user.js
-cp app/scripts/admin.js ./dist/scripts/
-cp app/scripts/user.js ./dist/scripts/
+gulp || { exit 1; }
 cd ./dist
 tar -zcvf ../starbucks.tar.gz .
 cd ..
-scp -i ~/.ssh/ICEsoft_Linux_Test_Key_Pair.pem starbucks.tar.gz ubuntu@$HOST:~/.
+scp -i ~/.ssh/ICEsoft_Linux_Test_Key_Pair.pem starbucks.tar.gz ubuntu@$HOST:~/. || { exit 1; }
 ssh -i ~/.ssh/ICEsoft_Linux_Test_Key_Pair.pem ubuntu@$HOST
 
 # sudo tar -zxvf ./starbucks.tar.gz -C /usr/share/nginx/html/static/demos/starbucks
